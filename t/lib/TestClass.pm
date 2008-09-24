713365e8e4c792f1bb14c48d3b4586d06adae09c
Florian Ragwitz <rafl@debian.org>
2008-09-24 04:58:47 +0200

    Test invocant naming.

--- t/lib/TestClass.pm
+++ t/lib/TestClass.pm
@@ -5,11 +5,11 @@ package TestClass;
 
 use MooseX::Method::Signatures;
 
-method new (Str $foo, Int $bar = 42) {
+method new ($class: Str $foo, Int $bar = 42) {
     return bless {
         foo => $foo,
         bar => $bar,
-    } => $self;
+    } => $class;
 }
 
 method foo {
