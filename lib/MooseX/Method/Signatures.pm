5d57efa773e4f3b93153b89a55d51d5cc93994ee
Florian Ragwitz <rafl@debian.org>
2008-10-12 03:50:35 +0200

    Version 0.05.

--- lib/MooseX/Method/Signatures.pm
+++ lib/MooseX/Method/Signatures.pm
@@ -11,7 +11,7 @@ use Perl6::Signature;
 use Moose::Util::TypeConstraints ();
 use MooseX::Meta::Signature::Combined;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 our ($Declarator, $Offset);
 
