173a2cb4c07c897e928ca763fa06638297a38b74
Florian Ragwitz <rafl@debian.org>
2008-10-14 05:49:45 +0200

    Version 0.06.

--- lib/MooseX/Method/Signatures.pm
+++ lib/MooseX/Method/Signatures.pm
@@ -12,7 +12,7 @@ use Moose::Meta::Method;
 use Moose::Util::TypeConstraints ();
 use MooseX::Meta::Signature::Combined;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 our ($Declarator, $Offset);
 
