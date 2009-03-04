97e9bdea5f57413d4c8a1cc5fda355233b87e104
Florian Ragwitz <rafl@debian.org>
2009-03-04 03:12:48 +0100

    Version 0.12.

--- lib/MooseX/Method/Signatures.pm
+++ lib/MooseX/Method/Signatures.pm
@@ -12,7 +12,7 @@ use MooseX::Method::Signatures::Meta::Method;
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 extends qw/Moose::Object Devel::Declare::MethodInstaller::Simple/;
 
