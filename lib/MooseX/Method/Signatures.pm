19293140f11964d0e04bf684593f9783e5ce180c
Florian Ragwitz <rafl@debian.org>
2009-08-17 12:14:10 +0200

    Version 0.20.

--- lib/MooseX/Method/Signatures.pm
+++ lib/MooseX/Method/Signatures.pm
@@ -19,7 +19,7 @@ use aliased 'Devel::Declare::Context::Simple', 'ContextSimple';
 
 use namespace::autoclean;
 
-our $VERSION = '0.19';
+our $VERSION = '0.20';
 
 has package => (
     is            => 'ro',
